1c8c69a2f3876b7b96eb2ba78c52d67867e6c98b
Kimi Chen <kimichen13@gmail.com>
2022-01-17 10:18:53 +0800

    Project Initial.

---
+++ src/main/java/com/qqviaja/jbpm/JbpmDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qqviaja.jbpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JbpmDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JbpmDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/qqviaja/jbpm/JbpmDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqviaja.jbpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbpmDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
